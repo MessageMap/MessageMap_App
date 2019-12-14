1c3ee722e715f899d58d94e617cf5512d551de2c
Ben Adams <ben@messagemap.io>
2019-12-14 07:53:12 -0500

    Change user bash commands

--- bin/moduser.sh
+++ bin/moduser.sh
@@ -1,1 +1,1 @@
-curl -v -X PUT -d 'user='`echo "$1" | openssl enc -base64 -d`'&password='`echo "$2" | openssl enc -base64 -d`''  http://localhost:8080/admin/moduser
+curl -v -X PUT -d 'user=\'`echo "$1" | openssl enc -base64 -d`\'&password=\'`echo "$2" | openssl enc -base64 -d`\''  http://localhost:8080/admin/moduser
